ce45d6fab2a73e618dfee09e561e5ee7050e18d9
webdatavisualdev <cli698638@gmail.com>
2017-12-21 07:33:53 -0800

    changed bar z-index

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ setTimeout(init, 100);
 
 function init() {
     $("body").append("<div class='js-widget-header'></div>");
-    $('.js-widget-header').css({position: 'fixed', top: '-60px', width: '100%', height: '60px', background: 'rgba(73,82,89,0.2)', 'z-index': '1005', left: '0', opacity: '0', 'padding': '0 40px'});
+    $('.js-widget-header').css({position: 'fixed', top: '-60px', width: '100%', height: '60px', background: 'rgba(73,82,89,0.2)', 'z-index': '9999', left: '0', opacity: '0', 'padding': '0 40px'});
     $('.js-widget-header').append('<a class="js-widget-btn-smart">Smart Bar</a>');
     $('.js-widget-btn-smart').css({'border-radius': '5px', height: '46px', 'font-size': '1.375rem', 'line-height': '46px', color: '#65717b', 'margin-top': '7px', padding: '0 15px', display: 'inline-block', 'background-color': '#ffffff', 'cursor': 'pointer'});
     $('.js-widget-header').animate({top: '0', opacity: 1}, 500);
